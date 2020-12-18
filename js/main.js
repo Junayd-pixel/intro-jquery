98a7dab14744b470b84155e5b8ee075b3dbf1db0
Junayd <junayd.guelmani18@gmail.com>
2020-12-18 09:33:12 +0100

    Premier commit jquery

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function() {
+    var $list;
+    $list = $('ul');
+    $list.on('click', 'li', function() {
+        var $this = $(this);
+        $this.slideUp(650);
+
+    });
+
+});
+
+
+$(document).ready(function(){
+    //Lors d'un clic sur le div...
+    $("img").click(function(){
+        //...On inverse les classes de li
+        $("li").toggleClass("afficher cacher");
+        //Les éléments avec .afficher seront visibles
+        $(".afficher1,.afficher2,.afficher3").show();
+        //Les éléments avec .cacher seront cachés
+        $(".cacher").hide();
+    });
+});
